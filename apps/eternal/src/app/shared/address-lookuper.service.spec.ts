db01bf2c39bae913355e86e290feec4476e3a744
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-04-28 10:16:03 +0200

    add unit tests for address lookuper

--- apps/eternal/src/app/shared/address-lookuper.service.spec.ts
+++ apps/eternal/src/app/shared/address-lookuper.service.spec.ts
@@ -1,16 +1,46 @@
-import {AddressLookuper} from "./address-lookuper.service";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { fakeAsync } from '@angular/core/testing';
+import { createSpyFromClass } from 'jest-auto-spies';
+import { asyncScheduler, firstValueFrom, of, scheduled } from 'rxjs';
+import { AddressLookuper } from './address-lookuper.service';
+import { assertType } from './assert-type';
 
 describe('Address Lookuper', () => {
-  it('should pass addresses in the constructor', () => {
-    const addresses = ['Domgasse 15, 1010 Wien'];
-    const lookuper = new AddressLookuper(() => addresses);
+  for (const { response, isValid } of [
+    { response: [true, {}], isValid: true },
+    { response: [], isValid: false }
+  ]) {
+    it(`should return ${isValid} for ${response}`, async () => {
+      const httpClientStub = assertType<HttpClient>({
+        get: () => scheduled([response], asyncScheduler)
+      });
 
-    expect(lookuper.lookup('Domgasse 5, 1010 Wien')).toBe(false);
-    expect(lookuper.lookup('Domgasse 15, 1010 Wien')).toBe(true);
-  });
+      const lookuper = new AddressLookuper(httpClientStub);
+      expect(await firstValueFrom(lookuper.lookup('Domgasse 5'))).toBe(isValid);
+    });
+  }
+
+  it('should use the HttpClient with the right parameters', fakeAsync(() => {
+    const httpClientMock = createSpyFromClass(HttpClient);
+    httpClientMock.get.mockReturnValue(of([]));
+
+    // const httpClientMock = {
+    //   get: jest.fn<Observable<string[]>, [string, { params: HttpParams }]>()
+    // };
+
+    const lookuper = new AddressLookuper(httpClientMock);
+    lookuper.lookup('Domgasse 5');
+
+    expect(httpClientMock.get).toHaveBeenCalledWith(
+      'https://nominatim.openstreetmap.org/search.php',
+      {
+        params: new HttpParams().set('format', 'jsonv2').set('q', 'Domgasse 5')
+      }
+    );
+  }));
 
   it('should throw an error if no street number is given', () => {
-    const lookuper = new AddressLookuper(() => []);
+    const lookuper = new AddressLookuper(assertType<HttpClient>());
 
     expect(() => lookuper.lookup('Domgasse')).toThrowError(
       'Could not parse address. Invalid format.'
--- apps/eternal/src/app/shared/address-lookuper.service.ts
+++ apps/eternal/src/app/shared/address-lookuper.service.ts
@@ -1,14 +1,21 @@
-import {parseAddress} from "./parse-address";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { parseAddress } from './parse-address';
 
+@Injectable({ providedIn: 'root' })
 export class AddressLookuper {
-  addresses :string[];
-  constructor(addressesSupplier: () => string[]) {
-    this.addresses = addressesSupplier();
-  }
+  constructor(private httpClient: HttpClient) {}
 
-  lookup(query: string): boolean {
+  lookup(query: string): Observable<boolean> {
     parseAddress(query);
-    return this.addresses.some((address) => address.startsWith(query));
+
+    return this.httpClient
+      .get<unknown[]>('https://nominatim.openstreetmap.org/search.php', {
+        params: new HttpParams().set('format', 'jsonv2').set('q', query)
+      })
+      .pipe(map((addresses) => addresses.length > 0));
   }
 
   // istanbul ignore next
